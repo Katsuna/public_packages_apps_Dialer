a9f541017e57371e39ab25f509269092391974b5
alkis <a.kafkis@gmail.com>
2018-06-30 07:51:57 +0300

    KatsunaCommon added.

--- Android.mk
+++ Android.mk
@@ -308,6 +308,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 	android-support-v7-appcompat \
 	android-support-v7-cardview \
 	android-support-v7-recyclerview \
+    KatsunaCommon
 
 LOCAL_JAVA_LIBRARIES := \
 	dialer-auto-value \
